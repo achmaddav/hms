629a2d836baff6c1452266abdc511a63bd826df9
achmaddav <achmaddavit92@gmail.com>
2026-02-17 08:22:48 +0700

    perubahan migrasi

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->enum('role', ['admin', 'receptionist', 'customer'])->default('customer');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
